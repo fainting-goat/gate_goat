930502f49a5aacba36754fcaea29c181928d3883
Kelsey Shannahan <kelsey.shannahan@gmail.com>
2018-05-15 17:41:53 -0400

    more visible errors

--- lib/gate_goat_web/views/registration_view.ex
+++ lib/gate_goat_web/views/registration_view.ex
@@ -1,6 +1,17 @@
 defmodule GateGoatWeb.RegistrationView do
   use GateGoatWeb, :view
 
+  def render_with_error(type, form, field) do
+
+    if form.errors[field] do
+      wrapper_class = "form-control form-errors-present"
+      apply(Phoenix.HTML.Form, type, [form, field, [class: wrapper_class]])
+    else
+      wrapper_class = "form-control"
+      apply(Phoenix.HTML.Form, type, [form, field, [class: wrapper_class]])
+    end
+  end
+
   def payment(%GateGoat.Events.Registration{feast_option: feast_option, camping_option: camping_option, event_id: event_id}) do
     get_event_fee(event_id) + get_feast_fee(event_id, feast_option) + get_camping_fee(event_id, camping_option)
   end
