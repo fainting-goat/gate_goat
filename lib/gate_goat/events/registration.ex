75caae6c419229fac6c9bf6e2ac29dbf317202ea
Kelsey Shannahan <kelsey.shannahan@gmail.com>
2018-09-18 23:49:29 -0400

    adding expiration date

--- lib/gate_goat/events/registration.ex
+++ lib/gate_goat/events/registration.ex
@@ -7,6 +7,7 @@ defmodule GateGoat.Events.Registration do
   schema "registrations" do
     field :group_name, :string
     field :membership_number, :string
+    field :membership_expiration_date, :date
     field :legal_name, :string
     field :sca_name, :string
     field :waiver, :boolean, default: false
@@ -20,9 +21,19 @@ defmodule GateGoat.Events.Registration do
   @doc false
   def changeset(registration, attrs) do
     registration
-    |> cast(attrs, [:sca_name, :legal_name, :membership_number, :group_name, :waiver, :feast_option, :camping_option])
-    |> validate_required([:sca_name, :legal_name, :membership_number, :group_name, :waiver, :feast_option, :camping_option])
+    |> cast(attrs, [:sca_name, :legal_name, :membership_number, :membership_expiration_date, :group_name, :waiver, :feast_option, :camping_option])
+    |> validate_required([:sca_name, :legal_name, :membership_number, :membership_expiration_date, :group_name, :waiver, :feast_option, :camping_option])
     |> validate_format(:membership_number, ~r/^\d+$/, [message: "Membership number must be a number."])
     |> validate_acceptance(:waiver, [message: "Waiver must be accepted."])
+    |> validate_expiration_date(:membership_expiration_date)
+  end
+
+  def validate_expiration_date(changeset, field) do
+    validate_change(changeset, field, fn _, date ->
+      case Date.compare(date, Date.utc_today()) do
+        :lt ->  [{field, "Your membership is expired.  Please use the non-member option."}]
+        _ -> []
+      end
+    end)
   end
 end
--- priv/repo/migrations/20180506152222_create_registrations.exs
+++ priv/repo/migrations/20180506152222_create_registrations.exs
@@ -6,6 +6,7 @@ defmodule GateGoat.Repo.Migrations.CreateRegistrations do
       add :sca_name, :string, null: false
       add :legal_name, :string, null: false
       add :membership_number, :string, null: false
+      add :membership_expiration_date, :date, null: false
       add :group_name, :string, null: true
       add :waiver, :boolean, default: false, null: false
       add :feast_option, :boolean, default: false
