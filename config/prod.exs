9feef36aba5fa0526eb7ed1706ae4f221f64345d
Kelsey Shannahan <kelsey.shannahan@gmail.com>
2019-04-12 08:45:00 -0400

    multiple dockers

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :gate_goat, GateGoatWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "localhost", port: 4001],
+  url: [host: "localhost", port: System.get_env("PORT")],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
